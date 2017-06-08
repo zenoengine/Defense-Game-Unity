b8c7d4d03f0fe9d2e420b0432d4956d220792738
ZenoAhn <zenoengine@gmail.com>
2017-06-08 18:18:26 +0900

    implemented wave manager, enemy movement

---
+++ Assets/Scripts/Monster/EnemyMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public Transform[] mPath = null;
+    public float mMoveSpeed = 1.0f;
+    public float mRotationSpeed = 10.0f;
+    public Animation mAnimation = null;
+    public float mWalkAnimationSpeed = 1.0f;
+
+    int mCurrentPathIndex = 0;
+    Transform mNexDestination = null;
+
+    void Start()
+    {
+        mPath = MapManager.instance.GetPathArry();
+
+        FindNextDesination();
+
+        if (mAnimation)
+        {
+            mAnimation["Walk"].speed = mWalkAnimationSpeed;
+            mAnimation.Play("Walk");
+        }
+    }
+
+    void FindNextDesination()
+    {
+        if (mCurrentPathIndex < mPath.Length)
+        {
+            mNexDestination = mPath[mCurrentPathIndex];
+
+            if (mCurrentPathIndex == 0)
+            {
+                transform.position = mNexDestination.position;
+            }
+
+            ++mCurrentPathIndex;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Update()
+    {
+        if (mNexDestination == null)
+        {
+            return;
+        }
+
+        Vector3 targetPos = mNexDestination.position;
+        Vector3 myPos = transform.position;
+        myPos.y = targetPos.y = 0.0f;
+
+        float distance = (targetPos - myPos).magnitude;
+
+        if (distance < 0.1f)
+        {
+            FindNextDesination();
+        }
+        else
+        {
+            Vector3 dir = mNexDestination.position - transform.position;
+            dir.y = 0.0f;
+            dir.Normalize();
+            transform.position += (dir * mMoveSpeed) * Time.deltaTime;
+
+            Quaternion from = transform.rotation;
+            Quaternion to = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Lerp(from, to, mRotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Wave
+{
+    public GameObject enemyPrefab = null;
+    public int spawnEnemyCount = 3;
+    public float spawnEnemyTime = 1.0f;
+}
+
+public class WaveManager : MonoBehaviour {
+
+    public int mWaveCount = 3;
+    public float mWaveDelayTime = 3.0f;
+
+    public List<Wave> mWaveList = new List<Wave>();
+
+    int mCurrentWaveIndex = 0;
+    bool mWaveProgress = false;
+
+    void Update()
+    {
+        if (mWaveProgress == false)
+        {
+            if (mCurrentWaveIndex < mWaveList.Count)
+            {
+                Wave wave = mWaveList[mCurrentWaveIndex];
+                StartCoroutine(WaveProcess(wave));
+                ++mCurrentWaveIndex;
+            }
+        }
+    }
+
+    IEnumerator WaveProcess(Wave wave)
+    {
+        mWaveProgress = true;
+        for (int i = 0; i < wave.spawnEnemyCount; ++i)
+        {
+            GameObject enemy = Instantiate(wave.enemyPrefab) as GameObject;
+            yield return new WaitForSeconds((wave.spawnEnemyTime));
+        }
+        yield return new WaitForSeconds(mWaveDelayTime);
+        mWaveProgress = false;
+    }
+}
